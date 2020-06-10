961da70301233367861386d7cfa141f687c8f45f
Balogzee <oluwaseunsb@gmail.com>
2020-06-10 12:51:15 +0100

    Added screenshot of completed app

--- lib/main.dart
+++ lib/main.dart
@@ -4,78 +4,36 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  Expanded noteBar(int noteNumber, Color color){
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        child:null,
+        onPressed: (){
+          final player = AudioCache();
+          player.play('note$noteNumber.wav');
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Expanded(
-                child: FlatButton(
-                  color: Colors.red,
-                  onPressed: (){
-                    final player = AudioCache();
-                    player.play('note1.wav');
-                  },
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.pink,
-                  onPressed: (){
-                    final player = AudioCache();
-                    player.play('note2.wav');
-                  },
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.purple,
-                  onPressed: (){
-                    final player = AudioCache();
-                    player.play('note3.wav');
-                  },
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.blue,
-                  onPressed: (){
-                    final player = AudioCache();
-                    player.play('note4.wav');
-                  },
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.green,
-                  onPressed: (){
-                    final player = AudioCache();
-                    player.play('note5.wav');
-                  },
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.orange,
-                  onPressed: (){
-                    final player = AudioCache();
-                    player.play('note6.wav');
-                  },
-                ),
-              ),
-              Expanded(
-                child: FlatButton(
-                  color: Colors.yellow,
-                  onPressed: (){
-                    final player = AudioCache();
-                    player.play('note7.wav');
-                  },
-                ),
-              ),
+              noteBar(1,Colors.red),
+              noteBar(2,Colors.orange),
+              noteBar(3,Colors.yellow),
+              noteBar(4,Colors.green),
+              noteBar(5,Colors.teal),
+              noteBar(6,Colors.blue),
+              noteBar(7,Colors.purple),
             ],
           ),
         ),
